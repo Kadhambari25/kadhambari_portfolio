ebf5a22df3c5b7dba3606a6e398a5a1e61bc7b33
Kadhambari25 <kadhambarisaravanan@gmail.com>
2025-11-01 13:12:21 +0530

    Rebuild and redeploy portfolio

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "20bea60d9c22e1f7dfbe50990034a79d
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "0741bda02381462d85d2891459c75705",
+"flutter_bootstrap.js": "c226efb8f7bddb8007582c8b6899a885",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
